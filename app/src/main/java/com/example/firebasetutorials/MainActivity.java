e007b516b710718e47ab653c9ca4d5cc02d13607
Arman05 <arifuzzamanarman@gmail.com>
2019-03-26 14:50:09 +0600

    Firebase ListView

--- app/src/main/java/com/example/firebasetutorials/MainActivity.java
+++ app/src/main/java/com/example/firebasetutorials/MainActivity.java
@@ -2,45 +2,64 @@ package com.example.firebasetutorials;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
-    private Button mFirebaseBtn;
     private DatabaseReference mDatabase;
 
-    private EditText mNameField;
+    private ListView mEventList;
+
+
+    public class Event {
+
+        String name;
+        String note;
+        Date date;
+
+        public Event(String name,int day, int month,int year, int hour, int min) {
+
+            this.name = name;
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(year+2000,month-1,day,hour,min);
+            this.date = calendar.getTime();
+            Log.i("DATE",this.date.toString());
+        }
+
+        public Event(String name,int day, int month,int year, int hour, int min,String note) {
+
+            this.name = name;
+            this.note = note;
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(year+2000,month-1,day,hour,min);
+            this.date = calendar.getTime();
+            Log.i("DATE",this.date.toString());
+        }
+
+    }
+
+
+    public void click(View view){
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mEventList = (ListView)  findViewById(R.id.eventList);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mFirebaseBtn = (Button) findViewById(R.id.firebase_btn);
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-        //mDatabase = FirebaseDatabase.getInstance().getReference().child("User_01");
-
-        mNameField = (EditText) findViewById(R.id.name_field);
-
-        mFirebaseBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                //1 - Create child in root object
-                //2- Assign some value to the child object
-
-                String name = mNameField.getText().toString().trim();
-
-                //mDatabase.child("Name").setValue(name);
-
-                mDatabase.push().setValue(name);
-
-            }
-        });
     }
 }
